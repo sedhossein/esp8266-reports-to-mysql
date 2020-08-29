10790fce82e155449d6ee9bd6fc336c993ab04ff
S.Hossein Hosseini <26527879+sedhossein@users.noreply.github.com>
2020-08-29 15:42:52 +0430

    Improve comments and whitespaces

--- flowMeter.php
+++ flowMeter.php
@@ -1,28 +1,22 @@
 <?php
 
+// TODO: you can add your validations here
 $data = $_GET['data'];
-//Validate Your Data => if it is invalid => die();
 
-
-// TODO
+// TODO: enter your own database data
 $servername = "localhost";
 $username = "YOUR USERNAME";
 $password = "PASS";
 $dbname = "your database name";
 
-
 // Create connection to mysql server
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-
-// Check connection
-if ($conn->connect_error) 
+if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+}
 
-
-$sql = "INSERT INTO YourTableName (data)
-VALUES ($data)";
-
+$sql = "INSERT INTO YourTableName (data) VALUES ($data)";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
@@ -30,5 +24,3 @@ if ($conn->query($sql) === TRUE) {
 }
 
 $conn->close();
-
-?>
